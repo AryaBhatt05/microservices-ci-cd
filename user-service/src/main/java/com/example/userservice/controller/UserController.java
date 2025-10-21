a8602d1f84b012c4453f1e4348c42410cdd90f25
Arya Bhatt <111279320+AryaBhatt05@users.noreply.github.com>
2025-10-21 14:17:58 +0530

    Initial microservices

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/api/users/hello")
+    public String hello() {
+        return "Hello from User Service";
+    }
+}
